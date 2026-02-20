7d90d12c24a9d5fa1d23cb6d76b105b01820357e
lucadibelardino <luca.dibelardino@gmail.com>
2026-02-20 03:09:23 +0100

    Fix: Remove unused Contact import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import Gallery from './components/Gallery';
-import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 const App: React.FC = () => {
